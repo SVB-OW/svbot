730aeaf4bde7256a4ad90b8dfb5b83442201c494
Florian Dendorfer <flo.dendorfer@gmail.com>
2021-09-03 14:43:34 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,11 +3,9 @@ const dbTest = 'svbot-test';
 const prefixProd = '!';
 const prefixTest = ',';
 
-console.log('DISCORD_TOKEN', process.env.DISCORD_TOKEN)
 export const isProd = process.env.NODE_ENV === 'production';
 export const discordToken = process.env.DISCORD_TOKEN;
-export const mongoUri =
-  'mongodb+srv://svbot:ZHCenGjnTpK9ZcqX@svbotcluster.1h7rr.mongodb.net/svbot?retryWrites=true&w=majority';
+export const mongoUri = process.env.MONGO_URI;
 export const dbLive = isProd ? dbProd : dbTest;
 export const prefixLive = isProd ? prefixProd : prefixTest;
 export const btagRegex = new RegExp(/.{3,}#[0-9]{4,}/);
