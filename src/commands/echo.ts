e8a972a0d8045f3278b8f9efb54d81cb4588d173
Dogers <Dogers@users.noreply.github.com>
2022-09-25 12:43:15 +0100

    Add echo command to test slash arguments

---
+++ src/commands/echo.ts
@@ -0,0 +1,11 @@
+import { Command } from '../types'
+import { PermissionFlagsBits } from "discord.js";
+
+module.exports = new Command({
+	name: 'echo',
+	description: 'Replies with the same params as you give it',
+	allowedPermissions: PermissionFlagsBits.ManageEvents,
+	async execute({ ia }) {
+		await ia.reply(ia.options.data.toString())
+	},
+})
