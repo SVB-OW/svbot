06f8aa27429213d21c9456de882c3461d94e402f
Dogers <Dogers@users.noreply.github.com>
2022-09-25 16:35:44 +0100

    Convert update to slash

--- src/commands/update.ts
+++ src/commands/update.ts
@@ -11,30 +11,31 @@ module.exports = new Command({
 		{ name: 'discordIds', required: true },
 	],
 	allowedChannels: ['bot-commands'],
-	async execute({ msg, args, mongoSignups }) {
-		if (args.length < 3) throw new ClientError(msg, 'Too few arguments. Format is !update <property> <value> <discordIds...>')
+	async execute({ ia, mongoSignups }) {
+		if (ia.options.data.length < 3) throw new ClientError(ia, 'Too few arguments. Format is !update <property> <value> <discordIds...>')
 
-		if (!new Signup().hasOwnProperty(args[0])) throw new ClientError(msg, 'Property does not exist')
+		let propVal = ia.options.data[0].value.toString()
+		if (!new Signup().hasOwnProperty(propVal)) throw new ClientError(ia, 'Property does not exist')
 
-		let newVal = args[1]
+		let newVal = ia.options.data[1].value.toString()
 		let updateRoles = false
-		if (['tankRank', 'damageRank', 'supportRank'].includes(args[0])) {
-			if (!rankResolver(args[1])) throw new ClientError(msg, 'Invalid rank')
-			newVal = rankResolver(args[1]) as string
+		if (['tankRank', 'damageRank', 'supportRank'].includes(propVal)) {
+			if (!rankResolver(newVal)) throw new ClientError(ia, 'Invalid rank')
+			newVal = rankResolver(newVal) as string
 			updateRoles = true
 		}
 
-		let userIds = args.slice(2)
+		let userIds = ia.options.data.slice(2)
 
 		await userIds.forEach(async (id) => {
 			const foundUser = await mongoSignups.findOne({ discordId: id })
-			if (!foundUser) throw new ClientError(msg, `Signup for ${id} was not found`)
+			if (!foundUser) throw new ClientError(ia, `Signup for ${id} was not found`)
 
-			foundUser[args[0]] = newVal
+			foundUser[propVal] = newVal
 			mongoSignups.updateOne({ discordId: id }, { $set: foundUser })
 
-			const member = await msg.guild.members.fetch(foundUser.discordId)
-			if (member.roles.cache.find((r) => r.name === 'Admin')) throw new ClientError(msg, 'Roles for admins cannot be changed automatically')
+			const member = await ia.guild.members.fetch(foundUser.discordId)
+			if (member.roles.cache.find((r) => r.name === 'Admin')) throw new ClientError(ia, 'Roles for admins cannot be changed automatically')
 
 			if (updateRoles) {
 				// Remove all rank roles (doesn't work with admins)
@@ -45,14 +46,14 @@ module.exports = new Command({
 				)
 
 				// Assign rank roles on confirm
-				if (foundUser.tankRank !== '-') member.roles.add(msg.guild.roles.cache.find((r) => r.name.toUpperCase() === foundUser.tankRank) as Role)
+				if (foundUser.tankRank !== '-') member.roles.add(ia.guild.roles.cache.find((r) => r.name.toUpperCase() === foundUser.tankRank) as Role)
 
-				if (foundUser.damageRank !== '-') member.roles.add(msg.guild.roles.cache.find((r) => r.name.toUpperCase() === foundUser.damageRank) as Role)
+				if (foundUser.damageRank !== '-') member.roles.add(ia.guild.roles.cache.find((r) => r.name.toUpperCase() === foundUser.damageRank) as Role)
 
-				if (foundUser.supportRank !== '-') member.roles.add(msg.guild.roles.cache.find((r) => r.name.toUpperCase() === foundUser.supportRank) as Role)
+				if (foundUser.supportRank !== '-') member.roles.add(ia.guild.roles.cache.find((r) => r.name.toUpperCase() === foundUser.supportRank) as Role)
 			}
 
-			await msg.channel.send({
+			await ia.reply({
 				embeds: [
 					new EmbedBuilder()
 						.setTitle('Updated signup')
