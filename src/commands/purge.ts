c94f14a8776b89737e594757efdc3e3af2a6eb81
Dogers <Dogers@users.noreply.github.com>
2022-09-25 23:12:06 +0100

    Convert purge to slash

--- src/commands/purge.ts
+++ src/commands/purge.ts
@@ -1,20 +1,21 @@
 import { Command, ClientError } from '../types'
-import { PermissionFlagsBits } from 'discord.js'
+import { ChannelType, PermissionFlagsBits, TextChannel } from 'discord.js'
 
 module.exports = new Command({
 	name: 'purge',
 	description: 'Deletes max 100 previous messages in the channel',
-	allowedPermissions: PermissionFlagsBits.Administrator,
+	allowedPermissions: PermissionFlagsBits.ManageMessages,
 	props: [{ name: 'number', required: false }],
 	async execute({ ia }) {
 
 		let num = 100
-		if (ia.options.data.length == 1){
+		if (ia.options.data.length == 1) {
 			num = Number.parseInt(ia.options.data[0].value!.toString())
 		}
 		if (num < 1 || num > 100) throw new ClientError(ia, 'Number must be in range 1-100')
 
-		await ia.channel!.bulkDelete(num)
-		ia.reply(num + ' messages have been deleted').then((m) => setTimeout(() => m.delete(), 3000))
+		const messages = await ia.channel!.messages.fetch({ limit: num });
+		await (ia.channel! as TextChannel).bulkDelete(num, true)
+		await ia.reply({ content: num + ' messages deleted!', ephemeral: true })
 	},
 })
