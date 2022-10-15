e19d31b89221b6e02c7ff95573ba3b1f8432dae6
Dogers <Dogers@users.noreply.github.com>
2022-10-15 18:49:51 +0100

    getunconfirmed fixes

--- src/commands/getunconfirmed.ts
+++ src/commands/getunconfirmed.ts
@@ -11,26 +11,24 @@ module.exports = new Command({
 	allowedChannels: ['bot-commands'],
 	allowedPermissions: PermissionFlagsBits.ManageEvents,
 	async execute({ ia, mongoSignups }) {
-		const unconfirmCount = Math.max(ia.options.getNumber('count') || 5, 10)
+		const unconfirmCount = Math.min(ia.options.getNumber('count') || 5, 10)
 
 		const embed = new EmbedBuilder().setTitle('Unconfirmed users').setTimestamp()
-		if (ia.guild.iconURL()) embed.setThumbnail(ia.guild.iconURL() as string)
 
-		mongoSignups.find({ confirmedBy: "" }, { limit: unconfirmCount }).toArray().then(async (signups: Signup[]) => {
-			signups.forEach(s => {
-				embed.addFields([
-					{ name: 'DiscordID', value: s.discordId, inline: true },
-					{ name: 'BattleTag', value: s.battleTag, inline: true },
-					{ name: 'Screenshot', value: s.screenshot, inline: true }
-				])
-			})
-
-			if (embed.toJSON().hasOwnProperty("fields") === false) {
-				await ia.reply("No more unconfirmed users left!")
-			} else {
-				embed.setFooter({ text: "Use /confirm as normal to confirm these users" })
-				await ia.reply({ embeds: [embed] })
-			}
+		const signups = await mongoSignups.find({ confirmedBy: '' }, { limit: unconfirmCount }).toArray()
+		signups.forEach(s => {
+			embed.addFields([
+				{ name: 'DiscordID', value: s.discordId, inline: true },
+				{ name: 'BattleTag', value: s.battleTag, inline: true },
+				{ name: 'Screenshot', value: s.screenshot, inline: true }
+			])
 		})
+
+		if (!signups.length) {
+			await ia.reply("No more unconfirmed users left!")
+		} else {
+			embed.setFooter({ text: "Use /confirm as normal to confirm these users" })
+			await ia.reply({ embeds: [embed] })
+		}
 	},
 })
