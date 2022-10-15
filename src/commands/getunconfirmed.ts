575d85dec9400fae799b4c5cb21920cf399f85f9
Florian Dendorfer <flo.dendorfer@gmail.com>
2022-10-15 20:08:41 +0200

    remove unused import

--- src/commands/getunconfirmed.ts
+++ src/commands/getunconfirmed.ts
@@ -1,13 +1,11 @@
-import { Command, Signup } from '../types'
+import { Command } from '../types'
 import { EmbedBuilder } from 'discord.js'
 import { PermissionFlagsBits } from 'discord.js'
 
 module.exports = new Command({
 	name: 'getunconfirmed',
 	description: 'Gets X unconfirmed users',
-	props: [
-		{ name: 'count', required: false },
-	],
+	props: [{ name: 'count', required: false }],
 	allowedChannels: ['bot-commands'],
 	allowedPermissions: PermissionFlagsBits.ManageEvents,
 	async execute({ ia, mongoSignups }) {
@@ -20,14 +18,14 @@ module.exports = new Command({
 			embed.addFields([
 				{ name: 'DiscordID', value: s.discordId, inline: true },
 				{ name: 'BattleTag', value: s.battleTag, inline: true },
-				{ name: 'Screenshot', value: s.screenshot, inline: true }
+				{ name: 'Screenshot', value: s.screenshot, inline: true },
 			])
 		})
 
 		if (!signups.length) {
-			await ia.reply("No more unconfirmed users left!")
+			await ia.reply('No more unconfirmed users left!')
 		} else {
-			embed.setFooter({ text: "Use /confirm as normal to confirm these users" })
+			embed.setFooter({ text: 'Use /confirm as normal to confirm these users' })
 			await ia.reply({ embeds: [embed] })
 		}
 	},
