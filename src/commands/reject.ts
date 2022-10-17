3b18f340c33b5ebbc0892e3d7f00a48905d9ca17
Florian Dendorfer <flo.dendorfer@gmail.com>
2022-10-17 19:47:30 +0200

    Untested, but should work :)

--- src/commands/reject.ts
+++ src/commands/reject.ts
@@ -1,5 +1,7 @@
 import { ClientError, Command } from '../types'
+import type { Role, TextChannel } from 'discord.js'
 import { PermissionFlagsBits } from 'discord.js'
+import { rankResolver } from '../helpers'
 
 module.exports = new Command({
 	name: 'reject',
@@ -10,9 +12,33 @@ module.exports = new Command({
 	async execute({ ia, mongoSignups }) {
 		const discordId = ia.options.getString('discord_id', true)
 
-		const res = await mongoSignups.deleteOne({ discordId })
+		const signupChannel = ia.guild.channels.cache.find(c => c.name === 'signup') as TextChannel
+		if (!signupChannel) throw new ClientError(ia, 'Signup channel does not exist')
 
-		if (res.deletedCount === 0) throw new ClientError(ia, `Signup for ${discordId} was not found`)
-		ia.reply('Removed signup for ' + discordId)
+		const foundSignup = await mongoSignups.findOne({ discordId })
+		if (!foundSignup) throw new ClientError(ia, `Signup for ${discordId} was not found`)
+
+		const member = await ia.guild.members.fetch(foundSignup.discordId)
+
+		// Remove roles
+		await member.roles.remove(ia.guild.roles.cache.find(r => r.name.toUpperCase() === 'GAUNTLET') as Role)
+		await member.roles.set(
+			Array.from(member.roles.cache.values())
+				.filter(r => !rankResolver(r.name.toUpperCase().replace('GAUNTLET ', '')))
+				.map(r => r.id),
+		)
+
+		// Delete signup
+		await mongoSignups.deleteOne({ discordId })
+
+		// Delete message
+		await signupChannel.messages.fetch(foundSignup.signupMsgId)
+		const oldMsg = signupChannel.messages.cache.get(foundSignup.signupMsgId)
+		if (oldMsg) {
+			oldMsg.delete()
+			ia.reply(`Signup for ${member.displayName} successfully deleted`)
+		} else {
+			throw new ClientError(ia, 'Signup message could not be fetched, but was deleted from db anyway')
+		}
 	},
 })
