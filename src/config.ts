a8c7ac2c96a59f7e0a2a24571a1fdf69b7062176
Florian Dendorfer <flo.dendorfer@gmail.com>
2022-10-29 01:50:54 +0200

    Updated CI

--- src/config.ts
+++ src/config.ts
@@ -11,3 +11,4 @@ export const btagRegex = new RegExp(/.{3,}#[0-9]{4,}/)
 export const regionRegex = new RegExp(/^(EU|NA)$/, 'i')
 export const discordToken = process.env.DISCORD_TOKEN as string
 export const mongoUri = process.env.MONGO_URI as string
+export const prodErrorEmail = process.env.PROD_ERROR_EMAIL as string
